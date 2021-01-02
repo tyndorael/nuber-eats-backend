a3a56cf726500dee04bbf6914f3d83af9e9b059c
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2021-01-01 22:29:55 -0300

    add login

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -3,6 +3,7 @@ import { CoreEntity } from "src/common/entities/core.entity";
 import { Field, InputType, ObjectType, registerEnumType } from "@nestjs/graphql";
 import * as bcrypt from "bcrypt";
 import { InternalServerErrorException } from "@nestjs/common";
+import { IsEmail, IsEnum } from "class-validator";
 
 enum UserRole {
     Owner,
@@ -17,6 +18,7 @@ registerEnumType(UserRole, { name: "UserRole"});
 export class User extends CoreEntity {
     @Column()
     @Field(type => String)
+    @IsEmail()
     email: string;
 
     @Column()
@@ -28,12 +30,21 @@ export class User extends CoreEntity {
         enum: UserRole
     })
     @Field(type => UserRole)
+    @IsEnum(UserRole)
     role: UserRole;
 
     @BeforeInsert()
     async hashPassword(): Promise<void> {
         try {
             this.password = await bcrypt.hash(this.password, 10);
+        } catch (e) {
+            throw new InternalServerErrorException();
+        }
+    }
+
+    async checkPassword(aPassword: string): Promise<boolean> {
+        try {
+            return await bcrypt.compare(aPassword, this.password);
         } catch (e) {
             throw new InternalServerErrorException();
         }
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,5 +1,6 @@
 import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { CreateAccountInput, CreateAccountOutput } from "./dtos/create-account.dto";
+import { LoginInput, LoginOutput } from "./dtos/login.dto";
 import { User } from "./entities/user.entity";
 import { UsersService } from "./users.service";
 
@@ -15,17 +16,24 @@ export class UsersResolver {
     @Mutation(returns => CreateAccountOutput)
     async createAccount(@Args("input") createAccountInput: CreateAccountInput): Promise<CreateAccountOutput> {
         try {
-            const {ok, error} = await this.usersService.createAccount(createAccountInput);
-
-            return {
-                ok,
-                error,
-            }
+            return await this.usersService.createAccount(createAccountInput);
         } catch (error) {
             return {
                 ok: false,
                 error,
-            } 
+            };
+        }
+    }
+
+    @Mutation(returns => LoginOutput)
+    async login(@Args('input') loginInput: LoginInput): Promise<LoginOutput> {
+        try {
+            return await this.usersService.login(loginInput);
+        } catch (error) {
+            return {
+                ok: false,
+                error,
+            };
         }
     }
 }
