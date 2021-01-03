9e26f5d1fd0a93e80bae9e848422e9f1f92ceb50
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2021-01-03 13:19:09 -0300

    add jwt custom module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { RestaurantModule } from './restaurants/restaurants.module';
 import { UsersModule } from './users/users.module';
 import { CommonModule } from './common/common.module';
 import { User } from './users/entities/user.entity';
+import { JwtModule } from './jwt/jwt.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { User } from './users/entities/user.entity';
         DB_USERNAME: Joi.string().required(),
         DB_PASSWORD: Joi.string().required(),
         DB_NAME: Joi.string().required(),
+        PRIVATE_KEY: Joi.string().required(),
       })
     }),
     TypeOrmModule.forRoot({
@@ -37,6 +39,9 @@ import { User } from './users/entities/user.entity';
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    JwtModule.forRoot({
+      privateKey: process.env.PRIVATE_KEY
     }),
     UsersModule,
     CommonModule,
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,11 +4,15 @@ import { Repository } from 'typeorm';
 import { CreateAccountInput } from './dtos/create-account.dto';
 import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from 'src/jwt/jwt.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
+    private readonly config: ConfigService,
+    private readonly jwtService: JwtService,
   ) {}
 
   async createAccount({
@@ -46,10 +50,10 @@ export class UsersService {
                 error: 'Wrong password',
             }
           }
-
+          const token = this.jwtService.sign(user.id);
           return {
             ok: true,
-            token: 'asdadasd'
+            token,
           }
       } catch (error) {
           return {
